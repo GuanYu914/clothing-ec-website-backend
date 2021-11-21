3519e895c060e743feb3edf2dc168b22e1f856ef
Emory <aa774281@gmail.com>
2021-11-21 20:44:48 +0800

    add php to get main categories

---
+++ handleGetCategories.php
@@ -0,0 +1,72 @@
+<?php
+/*  
+    response information
+    {
+      isSuccessful: STRING, 'failed' or 'successful'
+      data        : encoded JSON, response data
+      msg         : STRING, 'message'
+      detail      : STRING, 'detail message for msg' 
+    }
+*/
+
+require_once('conn.php');
+
+$query_type = $_GET['type'];
+if ($query_type === null) {
+  $response = array(
+    'isSuccessful' => 'failed',
+    'data'         => 'none',
+    'msg'          => "please use query string 'type'",
+    'detail'        => '',
+  );
+
+  $response = json_encode($response);
+  header('Content-Type: application/json;charset=utf-8');
+  echo $response;
+  die();
+}
+if ($query_type == 'main') {
+  // echo "receive main";
+  $stmt = $conn->prepare('SELECT category_id as id, name FROM categories ORDER BY category_id ASC');
+  $res = $stmt->execute();
+
+  if (!$res) {
+    $response = array(
+      'isSuccessful' => 'failed',
+      'data'         => 'none',
+      'msg'          => "can't get categories info from database",
+      'detail'        => $stmt->error,
+    );
+    $response = json_encode($response);
+    header('Content-Type: application/json;charset=utf-8');
+    echo $response;
+    die();
+  }
+
+  $res = $stmt->get_result();
+  $data = array();
+  while ($row = $res->fetch_assoc()) {
+    array_push(
+      $data,
+      array(
+        'id'   => $row['id'],
+        'name' => $row['name'],
+      )
+    );
+  }
+
+  $response = array(
+    'isSuccessful'  => 'successful',
+    'data'          => $data,
+    'msg'           => 'none',
+    'detail'        => 'none'
+  );
+  $response = json_encode($response);
+  header('Content-Type: application/json;charset=utf-8');
+  echo $response;
+  die();
+}
+
+if ($query_type == 'detail') {
+  // echo "receive detail";
+}
