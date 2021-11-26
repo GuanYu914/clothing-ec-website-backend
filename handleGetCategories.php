36cd8efe18fb40912eeca0da0780c34473113786
Emory <aa774281@gmail.com>
2021-11-26 18:46:38 +0800

    add condition of query string 'detail'

--- handleGetCategories.php
+++ handleGetCategories.php
@@ -69,4 +69,41 @@ if ($query_type == 'main') {
 
 if ($query_type == 'detail') {
   // echo "receive detail";
+  $stmt = $conn->prepare('SELECT category_id as id, name, sub_category as category FROM categories ORDER BY category_id ASC');
+  $res = $stmt->execute();
+
+  if (!$res) {
+    $response = array(
+      'isSuccessful' => 'failed',
+      'data'         => 'none',
+      'msg'          => "can't get categories info from database",
+      'detail'        => $stmt->error,
+    );
+    $response = json_encode($response);
+    header('Content-Type: application/json;charset=utf-8');
+    echo $response;
+    die();
+  }
+
+  $res = $stmt->get_result();
+  $data = array();
+  while ($row = $res->fetch_assoc()) {
+    array_push($data, array(
+      'id'        => $row['id'],
+      'name'      => $row['name'],
+      'category'  => $row['category']
+    ));
+  }
+
+  $response = array(
+    'isSuccessful'  => 'successful',
+    'data'          => $data,
+    'msg'           => 'none',
+    'detail'        => 'none',
+  );
+
+  $response = json_encode($response);
+  header('Content-Type: application/json;charset=utf-8');
+  echo $response;
+  die();
 }
